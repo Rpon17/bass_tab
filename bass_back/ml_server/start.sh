de0ffe619d4edea7c214f9788129e306550113a9
Rpon17 <kaizerjinwon@gmail.com>
2026-03-18 04:36:13 +0900

    리얼참참참트루

--- bass_back/ml_server/start.sh
+++ bass_back/ml_server/start.sh
@@ -1,7 +1,8 @@
-#!/bin/bash
+# 1. 현재 폴더(ml_server)를 파이썬 경로에 추가해!
+export PYTHONPATH=$PYTHONPATH:$(pwd)
 
-# 1. ML 분석 워커(ml_worker) 실행
+# 2. 워커 실행
 python app/worker/ml_worker.py &
 
-# 2. ML API 서버 실행
+# 3. 메인 서버 실행
 uvicorn app.main:app --host 0.0.0.0 --port 10000
